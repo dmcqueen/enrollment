e557c47790d57bea6c3cf8df3ed0a42c992e8f47
Dave McQueen <dmcqueen@gmail.com>
2012-07-24 21:37:41 -0700

    Added an initial negative test case (failing as of this checkin)

--- spec/integration/create_student_spec.rb
+++ spec/integration/create_student_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'create student page' do
+describe 'create student page with the happy case' do
   it 'lets the user create a student', :js => true do
     visit new_student_path
     fill_in 'Name', :with => 'J. Random Hacker'
@@ -9,5 +9,13 @@ describe 'create student page' do
     fill_in 'Ssn', :with => '000000000'
     click_on 'Create Student'  
     page.should have_content('Student was successfully created')
+  end
+end
+
+describe 'create student page with missing data' do
+  it 'ties to create a student with all fields missing', :js => true do
+    visit new_student_path
+    click_on 'Create Student'  
+    page.should have_content('fields required')
   end
 end
